f75294ab39bcf8994322a29cab0407f67fbe96be
Mohammad Osman Arrib <93909535+MohammadOsmanArrib@users.noreply.github.com>
2024-07-13 17:58:50 +0430

    Route and menus are created without design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="{{ asset('favicon_update.ico') }}" sizes="16x16" type="image/x-icon">
         <title inertia>{{config('app.name', 'CompromiseTech')}}</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/app.css">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
@@ -16,7 +17,7 @@
         @inertiaHead
     </head>
 
-    <body style="background-color:FFFFFF;" class="font-sans antialiased">
+    <body class="font-sans antialiased">
         @inertia
         
     </body>
